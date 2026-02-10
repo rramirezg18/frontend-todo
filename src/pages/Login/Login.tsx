4ce2e911a4de0acf568f8283f5e607eaa12d9b9d
rramirezg18 <rramirezg18@miumg.edu.gt>
2026-02-09 20:47:34 -0600

    aprendiendo formulario login goood

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,8 +1,52 @@
+import { useState } from "react";
+
 const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState<string | null>(null);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!username || !password) {
+      setError('Todos los campos son obligatorios');
+      return;
+    }
+
+    setError(null);
+
+    console.log('username: ', username);
+    console.log('password: ', password)
+  }
   return (
-    <div>
-      <label>Nombre de usuario</label>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Nombre de usuario</label>
+        <input
+          id="username"
+          type="text"
+          name="username"
+          autoComplete="username"
+          placeholder="Ingresa tu usuario"
+          value={username} onChange={(e) => setUsername(e.target.value)} />
+      </div>
+
+      <div>
+        <label htmlFor="password">Contraseña</label>
+        <input
+          id="password"
+          type="password"
+          name="password"
+          autoComplete="current-password"
+          placeholder="Ingresa tu contraseña"
+          value={password} onChange={(e) => setPassword(e.target.value)} />
+      </div>
+
+      {error && <p>{error}</p>}
+
+      <button type="submit">Iniciar sesión</button>
+
+    </form>
   );
 };
 
